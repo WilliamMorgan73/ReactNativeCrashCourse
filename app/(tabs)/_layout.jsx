3f452d998e54c1ccd05a24c2b7b6ff4735c93556
William Morgan <sghp78@durham.ac.uk>
2024-10-21 21:42:47 +0100

    Index nearly complete  1:06:22

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -13,7 +13,8 @@ const TabIcon = ({ icon, color, name, focused }) => {
         className="w-6 h-6"
       />
       <Text
-        className={'${focused ? "font-psemibold" : "font-pregular"} text-xs'}
+        className={`${focused ? "font-psemibold" : "font-pregular"} text-xs`}
+        style={{ color: color }}
       >
         {name}
       </Text>
@@ -27,6 +28,14 @@ const TabsLayout = () => {
       <Tabs
         screenOptions={{
           tabBarShowLabel: false,
+          tabBarActiveTintColor: "#FFA001",
+          tabBarInactiveTintColor: "#CDCDE0",
+          tabBarStyle: {
+            backgroundColor: "#161622",
+            borderTopWidth: 1,
+            borderTopColor: "#232533",
+            height: 84,
+          },
         }}
       >
         <Tabs.Screen
--- app/index.jsx
+++ app/index.jsx
@@ -1,13 +1,49 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { Image, Text, View, ScrollView } from "react-native";
 import { Link } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import { images } from "../constants";
+import CustomButton from "../components/CustomButton";
 
 export default function App() {
   return (
-    <View className = "flex-1 items-center justify-center bg-white">
-      <Text className = "text-3xl font-pblack">Aora!</Text>
-      <StatusBar style="auto" />
-      <Link href="/home" style = {{color: 'blue'}}>Go Home</Link>
+    // Should be SafeAreaView but not working with nativeWind
+    <View className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="w-full h-full items-center justify-center px-4">
+          <Image
+            source={images.logo}
+            className="w-[130px] h-[84px]"
+            resizeMode="contain"
+          />
+          <Image
+            source={images.cards}
+            className="max-w--[300px] w-full h-[300px]"
+            resizeMode="contain"
+          />
+          <View className="relative mt-3">
+            <Text className="text-3xl text-white font-bold text-center">
+              Discover Endless Possibilities With{" "}
+              <Text className="text-secondary-200">Aora</Text>
+            </Text>
+            <Image
+              source={images.path}
+              className="w-[136px] h-[15px] -top-2 -right-40"
+              resizeMode="contain"
+            />
+          </View>
+          <Text className="text-sm text-gray-100 mt-7 text-center font-pregular ">
+            Where creativity meets innovation: Embark on a journey of endless
+            possibilities with Aora.
+          </Text>
+
+          <CustomButton title = "Get Started"
+          handlePress={() => {}}
+          // Not being applied - Don't know why
+          containerStyles = "w-full mt-7"/>
+        </View>
+      </ScrollView>
     </View>
   );
 }
