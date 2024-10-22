d73629b73967e7b632cf06e860923aa197e671d1
William Morgan <sghp78@durham.ac.uk>
2024-10-22 16:51:49 +0100

    Got to 1:12:10. Changed layout for auth and started sign-in page

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,12 +1,15 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, ScrollView } from "react-native";
+import React from "react";
 
 const SignIn = () => {
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
-  )
-}
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        {/* If not centered change to min-h-[85vh]*/}
+        <View className="w-full justify-center h-full px-4 my-6"></View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
-export default SignIn
+export default SignIn;
